7b6db3133545e7aedf6ad193f08b77c733431ffa
szmozes <szmozes1999@gmail.com>
2024-09-18 05:06:13 +0200

    dependency upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import org.gradle.kotlin.dsl.withType
+
 plugins {
-    val kotlinVersion = "2.0.0-Beta4"
+    val kotlinVersion = "2.0.20"
 
     kotlin("jvm") version kotlinVersion
     id("com.diffplug.spotless") version "6.25.0"
@@ -41,4 +44,17 @@ kotlin {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version) || version.contains("-jre")
+    return isStable.not()
 }
