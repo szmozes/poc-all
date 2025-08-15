dba278b9006bee8ffe9ece3585d0551c300fa39b
szmozes <szmozes1999@gmail.com>
2025-08-15 22:32:19 +0200

    postgres docker

---
+++ docker/sql/create_databases.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE "auth_engine";
+GRANT ALL PRIVILEGES ON DATABASE "auth_engine" TO postgres;
+
+CREATE DATABASE "filter_specification";
+GRANT ALL PRIVILEGES ON DATABASE "filter_specification" TO postgres;
