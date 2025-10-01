f3d94de5a100c1856312654c838100744e92c44e
szmozes <szmozes1999@gmail.com>
2025-10-01 19:59:43 +0200

    StaticMapUtil

---
+++ benchmark/src/main/java/StaticMapUtil.java
@@ -0,0 +1,36 @@
+import java.util.Map;
+
+public class StaticMapUtil {
+    public static final Map<String, String> STATIC_MAP =
+            Map.of("A", "Apple",
+                    "B", "Banana",
+                    "C", "Cherry",
+                    "D", "Date",
+                    "E", "Eggplant");
+
+    public String toFruit(String letter) {
+        return STATIC_MAP.get(letter);
+    }
+
+    public String toFruit2(String letter) {
+        Map<String, String> map =
+                Map.of("A", "Apple",
+                        "B", "Banana",
+                        "C", "Cherry",
+                        "D", "Date",
+                        "E", "Eggplant");
+        return map.get(letter);
+    }
+
+    public static String toFruit3(String letter) {
+        class Local {
+            static final Map<String, String> MAP = Map.of("A", "Apple",
+                    "B", "Banana",
+                    "C", "Cherry",
+                    "D", "Date",
+                    "E", "Eggplant");
+        }
+        return Local.MAP.get(letter);
+    }
+
+}
