1b9a2f33aeafdd0370c039918b580a850bc0c533
szmozes <szmozes1999@gmail.com>
2025-10-01 20:40:10 +0200

    extract map

--- benchmark/src/main/java/StaticMapUtil.java
+++ benchmark/src/main/java/StaticMapUtil.java
@@ -1,36 +1,30 @@
 import java.util.Map;
 
 public class StaticMapUtil {
-    public static final Map<String, String> STATIC_MAP =
-            Map.of("A", "Apple",
-                    "B", "Banana",
-                    "C", "Cherry",
-                    "D", "Date",
-                    "E", "Eggplant");
+    public static final Map<String, String> STATIC_MAP = getMap();
 
     public static String toFruit(String letter) {
         return STATIC_MAP.get(letter);
     }
 
     public static String toFruit2(String letter) {
-        Map<String, String> map =
-                Map.of("A", "Apple",
-                        "B", "Banana",
-                        "C", "Cherry",
-                        "D", "Date",
-                        "E", "Eggplant");
+        Map<String, String> map = getMap();
         return map.get(letter);
     }
 
     public static String toFruit3(String letter) {
         class Local {
-            static final Map<String, String> MAP = Map.of("A", "Apple",
-                    "B", "Banana",
-                    "C", "Cherry",
-                    "D", "Date",
-                    "E", "Eggplant");
+            static final Map<String, String> MAP = getMap();
         }
         return Local.MAP.get(letter);
+    }
+
+    private static Map<String, String> getMap() {
+        return Map.of("A", "Apple",
+                "B", "Banana",
+                "C", "Cherry",
+                "D", "Date",
+                "E", "Eggplant");
     }
 
 }
