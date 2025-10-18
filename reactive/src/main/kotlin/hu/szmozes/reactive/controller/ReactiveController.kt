da822510dfe809255831aeabeaeba723e73611b0
szmozes <szmozes1999@gmail.com>
2025-10-18 20:40:09 +0200

    reactive functions

--- reactive/src/main/kotlin/hu/szmozes/reactive/controller/ReactiveController.kt
+++ reactive/src/main/kotlin/hu/szmozes/reactive/controller/ReactiveController.kt
@@ -3,6 +3,7 @@ package hu.szmozes.reactive.controller
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Hooks
 import reactor.core.publisher.Mono
 import reactor.core.scheduler.Schedulers
 import kotlin.random.Random
@@ -12,52 +13,69 @@ class ReactiveController {
 
     @GetMapping("/imperative")
     fun imperative(): ResponseEntity<List<List<Int>>> {
-        val results = listOf(
-            random1(),
-            random2(),
-            random3(),
-            random4(),
-            random5()
-        )
+        Hooks.onOperatorDebug()
+        val results = lists()
         return ResponseEntity.ok(results)
+    }
+
+    private fun lists(): List<List<Int>> {
+        return listOf(
+            random1().block()!!,
+            random2().block()!!,
+            random3().block()!!,
+            random4().block()!!,
+            random5().block()!!
+        )
     }
 
     @GetMapping("/reactive")
     fun reactive(): Mono<ResponseEntity<List<List<Int>>>> {
-        val mono1 = Mono.fromCallable { random1() }.subscribeOn(Schedulers.boundedElastic())
-        val mono2 = Mono.fromCallable { random2() }.subscribeOn(Schedulers.boundedElastic())
-        val mono3 = Mono.fromCallable { random3() }.subscribeOn(Schedulers.boundedElastic())
-        val mono4 = Mono.fromCallable { random4() }.subscribeOn(Schedulers.boundedElastic())
-        val mono5 = Mono.fromCallable { random5() }.subscribeOn(Schedulers.boundedElastic())
+        Hooks.onOperatorDebug()
+        return mono().map { ResponseEntity.ok(it) }
+    }
 
-        return Mono.zip(mono1, mono2, mono3, mono4, mono5)
+    private fun mono(): Mono<List<List<Int>>> {
+        return Mono.zip(random1(), random2(), random3(), random4(), random5())
             .map { tuple ->
-                ResponseEntity.ok(listOf(tuple.t1, tuple.t2, tuple.t3, tuple.t4, tuple.t5))
+                listOf(tuple.t1, tuple.t2, tuple.t3, tuple.t4, tuple.t5)
             }
     }
 
-    private fun random1(): List<Int> {
-        Thread.sleep(2000)
-        return List(5) { Random.nextInt(1, 101) }
+    private fun random1(): Mono<List<Int>> {
+        return Mono.fromCallable {
+            Thread.sleep(2000)
+            List(5) { Random.nextInt(1, 101) }
+        }.subscribeOn(Schedulers.boundedElastic())
     }
 
-    private fun random2(): List<Int> {
-        Thread.sleep(2000)
-        return List(5) { Random.nextInt(1, 101) }
+    private fun random2(): Mono<List<Int>> {
+        return Mono.fromCallable {
+            Thread.sleep(2000)
+            List(5) { Random.nextInt(1, 101) }
+        }.subscribeOn(Schedulers.boundedElastic())
     }
 
-    private fun random3(): List<Int> {
-        Thread.sleep(2000)
-        return List(5) { Random.nextInt(1, 101) }
+    private fun random3(): Mono<List<Int>> {
+        return Mono.fromCallable {
+            Thread.sleep(2000)
+            List(5) { Random.nextInt(1, 101) }
+        }.subscribeOn(Schedulers.boundedElastic())
     }
 
-    private fun random4(): List<Int> {
-        Thread.sleep(2000)
-        return List(5) { Random.nextInt(1, 101) }
+    private fun random4(): Mono<List<Int>> {
+        return Mono.fromCallable {
+            Thread.sleep(2000)
+            if (true) {
+                throw RuntimeException("Something went wrong!")
+            }
+            List(5) { Random.nextInt(1, 101) }
+        }.subscribeOn(Schedulers.boundedElastic())
     }
 
-    private fun random5(): List<Int> {
-        Thread.sleep(2000)
-        return List(5) { Random.nextInt(1, 101) }
+    private fun random5(): Mono<List<Int>> {
+        return Mono.fromCallable {
+            Thread.sleep(2000)
+            List(5) { Random.nextInt(1, 101) }
+        }.subscribeOn(Schedulers.boundedElastic())
     }
 }
