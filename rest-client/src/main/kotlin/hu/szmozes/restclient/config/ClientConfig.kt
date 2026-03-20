32904b4cda7ac565b50499d85573f4ac79618c2a
szmozes <szmozes1999@gmail.com>
2026-03-20 03:33:36 +0100

    remove group

--- rest-client/src/main/kotlin/hu/szmozes/restclient/config/ClientConfig.kt
+++ rest-client/src/main/kotlin/hu/szmozes/restclient/config/ClientConfig.kt
@@ -5,8 +5,5 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.service.registry.ImportHttpServices
 
 @Configuration
-@ImportHttpServices(
-    group = "post-client",
-    types = [PostClient::class],
-)
+@ImportHttpServices(PostClient::class)
 class ClientConfig
